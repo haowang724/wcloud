8e2209306db255299539efb6bbc5c00afeae3347
wanghao <623455172@qq.com>
2020-04-02 09:44:18 +0800

    添加uaa项目

---
+++ wcloud-uaa/src/test/java/com/cq/wh/wclouduaa/WcloudUaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cq.wh.wclouduaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcloudUaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
