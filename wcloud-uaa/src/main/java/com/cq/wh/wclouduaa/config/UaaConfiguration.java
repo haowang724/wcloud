92d1fc5573d027d82eed3ecae45ff72807fce49c
wanghao <623455172@qq.com>
2020-04-02 17:23:32 +0800

    uaa 授权服务器代码配置

---
+++ wcloud-uaa/src/main/java/com/cq/wh/wclouduaa/config/UaaConfiguration.java
@@ -0,0 +1,89 @@
+package com.cq.wh.wclouduaa.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import java.security.KeyPair;
+
+/**
+ * @Auther: wh
+ * @Date: 2020/4/2 13:48
+ * @Description:
+ */
+@Configuration
+@EnableAuthorizationServer
+public class UaaConfiguration extends AuthorizationServerConfigurerAdapter implements ApplicationContextAware {
+
+    @Autowired
+    public PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserDetailsService kiteUserDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        KeyPair keyPair = new KeyStoreKeyFactory(
+                new ClassPathResource("keystore.jks"), "foobar".toCharArray())
+                .getKeyPair("test");
+        converter.setKeyPair(keyPair);
+        return converter;
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("order-client")
+                .secret(passwordEncoder.encode("order-secret-8888"))
+                .authorizedGrantTypes("refresh_token", "authorization_code", "password")
+                .accessTokenValiditySeconds(3600)
+                .scopes("all")
+                .and()
+                .withClient("user-client")
+                .secret(passwordEncoder.encode("user-secret-8888"))
+                .authorizedGrantTypes("refresh_token", "authorization_code", "password")
+                .accessTokenValiditySeconds(3600)
+                .scopes("all");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+            throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(kiteUserDetailsService)
+                .accessTokenConverter(jwtAccessTokenConverter());;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer)
+            throws Exception {
+        oauthServer.tokenKeyAccess("permitAll()").checkTokenAccess(
+                "isAuthenticated()");
+    }
+}
